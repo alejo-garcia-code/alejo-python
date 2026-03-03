fd454d29b72177298f739f8e33c6e32ad1cb5bd3
alejo-garcia-code <agarciaberme@gmail.com>
2026-03-03 20:00:39 +0100

    Update greeting message with current time

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hola desde GitHub")
+import datetime
+
+nombre = "Programador"
+hora = datetime.datetime.now()
+
+print(f"¡Hola {nombre}!")
+print(f"El robot de GitHub ejecutó esto el: {hora}")
+
